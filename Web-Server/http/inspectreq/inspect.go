0e10211c2ad3cbd33693f6cf02b8d46e6cf8f388
shubh-man007 <randombro2077@gmail.com>
2025-07-15 23:59:10 +0530

    Added inspect request func

---
+++ Web-Server/http/inspectreq/inspect.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	ctx := context.TODO()
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://eblog.fly.dev/index.html", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Accept-Encoding", "gzip")
+	req.Header.Add("Accept-Encoding", "deflate")
+	req.Header.Set("User-Agent", "eblog/1.0")
+	req.Header.Set("some-key", "a value")
+	req.Header.Set("SOMe-KEY", "somevalue") // overwrites previous
+
+	req.Write(os.Stdout) // print full HTTP request
+}
