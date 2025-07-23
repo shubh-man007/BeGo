8e4efedcfd5f81230941c59d3dc5ba2887965b56
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-07-23 19:07:10 +0530

    Update api.go

--- GoBank/api.go
+++ GoBank/api.go
@@ -33,10 +33,10 @@ func makeHTTPHandleFunc(f APIFunc) http.HandlerFunc {
 
 type APIServer struct {
 	listenAddr string
-	storage    PostgresStorage
+	storage    *PostgresStorage
 }
 
-func NewAPIServer(listenAddr string, storage PostgresStorage) *APIServer {
+func NewAPIServer(listenAddr string, storage *PostgresStorage) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 		storage:    storage,
@@ -48,6 +48,7 @@ func (s *APIServer) Run() {
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
+	router.HandleFunc("/account/{id}/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("Server running on port: ", s.listenAddr)
 	http.ListenAndServe(s.listenAddr, router)
@@ -65,6 +66,9 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 	case "DELETE":
 		return s.handleDeleteAccount(w, r)
 
+	case "PUT":
+		return s.handleUpdateAccount(w, r)
+
 	default:
 		return fmt.Errorf("method not allowed: %s", r.Method)
 	}
@@ -80,6 +84,7 @@ func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) err
 	p := s.storage
 	acc, err := p.GetAccountByID(id)
 	if err != nil {
+		log.Printf("Could not fetch account[%d]", id)
 		return err
 	}
 	return WriteJSON(w, http.StatusOK, acc)
@@ -105,7 +110,7 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	p := s.storage
 	err = p.CreateAccount(&a)
 	if err != nil {
-		log.Printf("Cannot create account")
+		log.Printf("Could not create account[%d]", a.AccountID)
 		return err
 	}
 	return WriteJSON(w, http.StatusOK, a)
@@ -121,11 +126,67 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 	p := s.storage
 	err = p.DeleteAccount(id)
 	if err != nil {
+		log.Printf("Could not delete account[%d]", id)
 		return err
 	}
-	return WriteJSON(w, http.StatusOK, id)
+
+	del := "Account" + idStr + " has been deleted"
+	return WriteJSON(w, http.StatusOK, del)
+}
+
+func (s *APIServer) handleUpdateAccount(w http.ResponseWriter, r *http.Request) error {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Could not read request body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError) // HTTP 500
+		return err
+	}
+
+	defer r.Body.Close()
+
+	var a account
+	err = json.Unmarshal(body, &a)
+	if err != nil {
+		log.Printf("Could not unmarshal request body: %v", err)
+		w.WriteHeader(http.StatusBadRequest) // HTTP 400
+		return err
+	}
+
+	p := s.storage
+	err = p.UpdateAccount(&a)
+	if err != nil {
+		log.Printf("Could not update account[%d]", a.AccountID)
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, a)
+
 }
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return fmt.Errorf("invalid account id")
+	}
+
+	var req struct {
+		Amount float64 `json:"amount"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return fmt.Errorf("invalid request body: %v", err)
+	}
+	defer r.Body.Close()
+
+	acc, err := s.storage.GetAccountByID(id)
+	if err != nil {
+		return fmt.Errorf("account not found: %v", err)
+	}
+
+	updatedAcc, err := s.storage.TransferBalance(acc, req.Amount)
+	if err != nil {
+		return fmt.Errorf("could not update balance: %v", err)
+	}
+
+	return WriteJSON(w, http.StatusOK, updatedAcc)
 }
