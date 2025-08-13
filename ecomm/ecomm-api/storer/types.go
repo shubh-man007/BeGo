90fd768d9c655471e3f97cd17f5c0d4300dbf641
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-08-13 14:19:26 +0530

    Create types.go

---
+++ ecomm/ecomm-api/storer/types.go
@@ -0,0 +1,38 @@
+package storer
+
+import "time"
+
+type Product struct {
+	ID           int64      `db:"id"`
+	Name         string     `db:"name"`
+	Image        string     `db:"image"`
+	Category     string     `db:"category"`
+	Description  string     `db:"description"`
+	Rating       int64      `db:"rating"`
+	NumReviews   int64      `db:"num_reviews"`
+	Price        float32    `db:"price"`
+	CountInStock int64      `db:"count_in_stock"`
+	CreatedAt    time.Time  `db:"created_at"`
+	UpdatedAt    *time.Time `db:"updated_at"`
+}
+
+type Order struct {
+	ID            int64      `db:"id"`
+	PaymentMethod string     `db:"payment_method"`
+	TaxPrice      float64    `db:"tax_price"`
+	ShippingPrice float64    `db:"shipping_price"`
+	TotalPrice    float64    `db:"total_price"`
+	CreatedAt     time.Time  `db:"created_at"`
+	UpdatedAt     *time.Time `db:"updated_at"`
+	Items         []OrderItem
+}
+
+type OrderItem struct {
+	ID        int64   `db:"id"`
+	Name      string  `db:"name"`
+	Quantity  int64   `db:"quantity"`
+	Image     string  `db:"image"`
+	Price     float64 `db:"price"`
+	ProductID int64   `db:"product_id"`
+	OrderID   int64   `db:"order_id"`
+}
