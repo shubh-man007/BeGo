4e41b1a66250e186db202aa624329f03e6f27128
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-11-08 19:18:31 +0530

    Refactor request handling for specific paths

--- httpfromtcp/cmd/httpserver/main.go
+++ httpfromtcp/cmd/httpserver/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/shubh-man007/BeGo/httpfromtcp/internal/request"
@@ -13,6 +19,7 @@ import (
 )
 
 const port = 42069
+const CRLF = "\r\n"
 
 // Client template:
 const res400 = `<html>
@@ -49,22 +56,110 @@ func RequestPath(w *response.Writer, req *request.Request) {
 	path := req.RequestLine.RequestTarget
 	h := response.GetDefaultHeaders(0)
 
-	var body []byte
-	var stat response.StatusCode
+	body := []byte(res200)
+	stat := response.StatusOK
 
-	switch path {
-	case "/yourproblem":
+	if path == "/yourproblem" {
 		body = []byte(res400)
 		stat = response.StatusBadRequest
-
-	case "/myproblem":
+	} else if path == "/myproblem" {
 		body = []byte(res500)
 		stat = response.StatusInternalServerError
+	} else if path == "/video" {
+		file, err := os.Open("assets/warehouse.mp4") // Set file name accordingly.
+		if err != nil {
+			body = []byte(res500)
+			stat = response.StatusInternalServerError
+		}
+		defer file.Close()
 
-	default:
-		body = []byte(res200)
-		stat = response.StatusOK
+		info, err := file.Stat()
+		if err != nil {
+			body = []byte(res500)
+			stat = response.StatusInternalServerError
+		}
+
+		h := response.GetDefaultHeaders(int(info.Size()))
+		h.Replace("Content-Type", "video/mp4")
+
+		w.WriteStatusLine(response.StatusOK)
+		w.WriteHeaders(h)
+
+		buffer := make([]byte, 32*1024) // 32KB chunks
+		for {
+			n, err := file.Read(buffer)
+			if n > 0 {
+				w.WriteBody(buffer[:n])
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				body = []byte(res500)
+				stat = response.StatusInternalServerError
+			}
+		}
+	} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+		target := req.RequestLine.RequestTarget
+		res, err := http.Get("https://httpbin.org" + strings.TrimPrefix(target, "/httpbin"))
+		if err != nil {
+			body = []byte(res500)
+			stat = response.StatusInternalServerError
+		} else {
+			w.WriteStatusLine(response.StatusOK)
+
+			h.Delete("Content-Length")
+			h.Set("transfer-encoding", "chunked")
+			h.Replace("Content-Type", "text/plain")
+			h.Set("Trailer", "X-Content-SHA256")
+			h.Set("Trailer", "X-Content-Length")
+
+			w.WriteHeaders(h)
+			w.WriteTrailers(h)
+
+			var fullBody []byte
+
+			fmt.Printf(">> Proxy Response: \n")
+			for {
+				data := make([]byte, 32)
+				n, err := res.Body.Read(data)
+				if err != nil {
+					break
+				}
+				w.WriteBody([]byte(fmt.Sprintf("%x%s", n, CRLF)))
+				w.WriteBody(data[:n])
+				w.WriteBody([]byte(CRLF))
+
+				fullBody = append(fullBody, data[:n]...)
+
+				log.Printf("\n%s\n", string(data[:n]))
+			}
+			w.WriteBody([]byte(fmt.Sprintf("0%s%s", CRLF, CRLF)))
+
+			hash := sha256.Sum256(fullBody)
+			h.Set("X-Content-SHA256", hex.EncodeToString(hash[:]))
+			h.Set("X-Content-Length", strconv.Itoa(len(fullBody)))
+
+			w.WriteTrailers(h)
+
+			log.Println("\n-----------------")
+			return
+		}
 	}
+
+	// switch path {
+	// case "/yourproblem":
+	// 	body = []byte(res400)
+	// 	stat = response.StatusBadRequest
+
+	// case "/myproblem":
+	// 	body = []byte(res500)
+	// 	stat = response.StatusInternalServerError
+
+	// default:
+	// 	body = []byte(res200)
+	// 	stat = response.StatusOK
+	// }
 
 	h.Replace("Content-Length", strconv.Itoa(len(body)))
 	h.Replace("Content-Type", "text/html")
