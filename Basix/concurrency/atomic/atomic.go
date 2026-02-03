3f16cf0ae829c6cd8a45c151de8be6074bc214fa
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2026-02-03 23:45:53 +0530

    Implement User struct with atomic.Value for concurrency
    Added User struct and atomic.Value usage for storing and updating user data.

--- Basix/concurrency/atomic/atomic.go
+++ Basix/concurrency/atomic/atomic.go
@@ -6,6 +6,11 @@ import (
 	"sync/atomic"
 )
 
+type User struct {
+	Name string
+	Age  int
+}
+
 func main() {
 	var val int32 = 0
 	var wg sync.WaitGroup
@@ -24,4 +29,22 @@ func main() {
 
 	swapped := atomic.CompareAndSwapInt32(&val, 1000, 1001)
 	fmt.Printf("Swapped: %v, Value: %d\n", swapped, val)
+
+	// Store user struct
+	var value atomic.Value
+	user := User{Name: "Little John", Age: 30}
+	value.Store(user)
+
+	// Load and print.
+	current := value.Load().(User)
+	fmt.Printf("Current user: %+v\n", current)
+
+	// Update with new user data.
+	user2 := User{Name: "Vera", Age: 36}
+	value.Store(user2)
+
+	// Load and print updated data.
+	updated := value.Load().(User)
+	fmt.Printf("Updated user: %+v\n", updated)
+
 }
